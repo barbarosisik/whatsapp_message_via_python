2c20284ce0c4781d84f8f55af12b7a6260849c27
Barbaros IŞIK <57237644+barbarosisik@users.noreply.github.com>
2022-01-23 14:56:24 +0300

    Add files via upload

---
+++ whatsapp_message_via_python.py
@@ -0,0 +1,7 @@
+import pywhatkit as kit
+
+try:
+    kit.sendwhatmsg("+1**********", "Hello World!",00,00)
+    print("Message sent successfully!")
+except:
+    print("Something went wrong, try again.")
